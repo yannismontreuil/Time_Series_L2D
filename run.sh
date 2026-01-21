806e9b89d76a7da4b7e5c880a4fd362d1ab3386d
YuLetian <yu.letian66@gmail.com>
2026-01-21 21:32:52 +0800

    Config run script

--- run.sh
+++ run.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=time-series-l2d
-#SBATCH --output=time-series-l2d-%A_%a.out
-#SBATCH --error=time-series-l2d-%A_%a.err
+#SBATCH --output=time-series-l2d-%A.out
+#SBATCH --error=time-series-l2d-%A.err
 #SBATCH --mem=32G
 #SBATCH --time=16:00:00
 #SBATCH --mail-user=yuletian@u.nus.edu
 #SBATCH --mail-type=START,END,FAIL
-#
-# There are 5 learning rates and 8 beta (cost) settings, making 40 combinations.
-# Limit to 6 concurrent jobs with the %6 notation.
-#SBATCH --array=0-9
 
 # Change to the directory from which the job was submitted
 cd "${SLURM_SUBMIT_DIR}"
