21bea2b8bf0ca9c28e812e1e93b899ec4e25a162
YuLetian <yu.letian66@gmail.com>
2026-01-21 21:35:09 +0800

    Config run script

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@
 # Change to the directory from which the job was submitted
 cd "${SLURM_SUBMIT_DIR}"
 
-python3 slds_imm_router.py -c config/config_etth1.yaml > output_${SLURM_ARRAY_TASK_ID}.log 2>&1
+python3 slds_imm_router.py -c config/config_etth1.yaml > output_${SLURM_JOB_ID}.log 2>&1
